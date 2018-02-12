8328735dd6d819d3ac264ed6d75ab9eaf2e05405
Nickolaus D. Saint <nick@globus.org>
2018-02-12 09:35:07 -0800

    Removed dead code, added docs.

--- globus_portal_framework/search/urls.py
+++ globus_portal_framework/search/urls.py
@@ -1,16 +1,11 @@
 from django.urls import path
-from globus_portal_framework.search.views import (index, detail, mock_metadata,
-                                                  mock_overview)
-from globus_portal_framework.search import api
+from globus_portal_framework.search.views import (index, detail, mock_metadata)
 
 urlpatterns = [
     # We will likely use this at some point
     # path('admin/', admin.site.urls),
-    path('api/v1/search/<index>', api.search),
-    path('test/overview', mock_overview),
-    path('test/metadata', mock_metadata),
     path('detail-metadata/<path:subject>', mock_metadata,
          name='detail-metadata'),
     path('detail/<path:subject>', detail, name='detail'),
-    path('', index)
+    path('', index, name='search')
 ]
--- globus_portal_framework/search/views.py
+++ globus_portal_framework/search/views.py
@@ -6,6 +6,57 @@ from django.conf import settings
 
 
 def index(request):
+    """
+    Search the index configured in settings.SEARCH_INDEX with the queryparams
+    'q' for query, 'filter.<filter>' for facet-filtering, 'page' for pagination
+    If the user visits this page again without a search query, we auto search
+    for them again using their last query. If the user is logged in, they will
+    automatically do a credentialed search for Globus Search to return
+    confidential results. If more results than settings.SEARCH_RESULTS_PER_PAGE
+    are returned, they are paginated (Globus Search does the pagination, we
+    only do the math to calculate the offset).
+
+    Query Params
+    q: key words for the users search. Ex: 'q=foo*' will search for 'foo*'
+
+    filter.: Filter results on facets defined in settings.SEARCH_SCHEMA. The
+    syntax for filters using query params is:
+    '?filter.<filter_type>=<filter_value>, where 'filter.<filter_type>' is
+    defined in settings.SEARCH_SCHEMA and <filter_value> is any value returned
+    by Globus Search contained within search results. For example, we can
+    define a filter 'mdf.elements' in our schema, and use it to filter all
+    results containing H (Hydrogen).
+
+    page: Page of the search results. Number of results displayed per page is
+    configured in settings.SEARCH_RESULTS_PER_PAGE, and number of pages can be
+    controlled with settings.SEARCH_MAX_PAGES.
+
+    Templates:
+    The resulting page is templated using the 'search.html' template, and
+    'search-results.html' and 'search-facets.html' template components. The
+    context required for searches is shown here:
+
+    {'search': {
+        'facets': [
+            {'buckets': [{'field_name': 'mdf.resource_type',
+                          'value': 'record'}],
+                          'name': 'Resource Type'},
+                         <More Facets>...],
+        'pagination': {'current_page': 1, 'pages': [{'number': 1}]},
+        'search_results': [
+            {'subject': '<Globus Search Subject>'}
+            'fields': {
+                'titles': {'name': 'titles', 'value': '<Result Title>'},
+                           'version': {'name': 'version', 'value': '0.3.2'}},
+                           '<field_name>': {'name': '<display_name>', 'value':
+                                                        '<field_value>'}
+            }
+        ]
+    }
+
+    Example request:
+    http://myhost/?q=foo*&page=2&filter.my.special.filter=goodresults
+    """
     context = {}
     query = request.GET.get('q') or request.session.get('query')
     if query:
@@ -21,17 +72,9 @@ def index(request):
     return render(request, 'search.html', context)
 
 
-def mock_overview(request, subject='bar'):
-    filename = 'globus_portal_framework/search/data/mock-detail-overview.json'
-    with open(filename) as f:
-        data = json.loads(f.read())
-        detail_data = utils.default_search_mapper(data)
-        return render(request, 'detail-overview.html',
-                      {'detail_data': detail_data,
-                       'subject': subject})
-
-
 def mock_metadata(request, subject='bar'):
+    """Ignore this for now. It isn't being used and will be removed or heavily
+    modified."""
     filename = 'globus_portal_framework/search/data/mock-detail-metadata.json'
     with open(filename) as f:
         data = json.loads(f.read())
@@ -44,15 +87,24 @@ def mock_metadata(request, subject='bar'):
 
 def detail(request, subject):
     """
-    Load a page for showing details for a single search result.
-    Context for the fields must be provided, an example is as follows:
-    {
-        'fields':
-            {'datacite_field':
-                {'name': 'datacite_field', 'value': 'myid'},
-            },
-            <more fields>
-        'subject': 'unique_identifier'
+    Load a page for showing details for a single search result. The data is
+    exactly the same as the entries loaded by the index page in the
+    'search_results'. The template is ultimately responsible for which fields
+    are displayed. The only real functional difference between the index page
+    and the detail page is that it displays only a single result The
+    detail-overview.html template is used to render the page.
+
+    Example request:
+    http://myhost/detail/<subject>
+
+    Example context:
+    {'subject': '<Globus Search Subject>'},
+     'fields': {
+                'titles': {'name': 'titles', 'value': '<Result Title>'},
+                           'version': {'name': 'version', 'value': '0.3.2'}},
+                           '<field_name>': {'name': '<display_name>', 'value':
+                                                        '<field_value>'}
+            }
     }
     """
     client = utils.load_search_client(request.user)
