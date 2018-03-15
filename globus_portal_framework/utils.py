1bac810d9e3060b79f9d010ae3834389eedb4e4d
Nickolaus D. Saint <nick@globus.org>
2018-03-15 11:58:49 -0700

    Split functionality for loading tokens and loading clients

--- globus_portal_framework/utils.py
+++ globus_portal_framework/utils.py
@@ -1,7 +1,25 @@
 import globus_sdk
 
 
-def load_globus_client(user, client, token_name):
+def load_globus_access_token(user, token_name):
+    if user.is_authenticated:
+        tok_list = user.social_auth.get(provider='globus').extra_data
+        if token_name == 'auth.globus.org':
+            return tok_list['access_token']
+        if tok_list.get('other_tokens'):
+            service_tokens = {t['resource_server']: t
+                              for t in tok_list['other_tokens']}
+            service_token = service_tokens.get(token_name)
+            if service_token:
+                return service_token['access_token']
+            else:
+                raise ValueError('Attempted to load {} for user {}, but no '
+                                 'tokens existed with the name {}, only {}'
+                                 ''.format(token_name, user, token_name,
+                                           list(service_tokens.keys())))
+
+
+def load_globus_client(user, client, token_name, require_authorized=False):
     """Load a globus client with a given user and the name of the token. If
     the user is Anonymous (Not logged in), then an unauthenticated client is
     returned. If the client is logged in and the token is not found, a
@@ -18,19 +36,16 @@ def load_globus_client(user, client, token_name):
     An 'AnonymousUser' is not logged in, so they will get a regular search
     client and can only search on public data.
     """
-    if user.is_authenticated:
-        tok_list = user.social_auth.get(provider='globus').extra_data
-        if tok_list.get('other_tokens'):
-            service_tokens = {t['resource_server']: t
-                              for t in tok_list['other_tokens']}
-            service_token = service_tokens.get(token_name)
-            if service_token:
-                authorizer = globus_sdk.AccessTokenAuthorizer(
-                    service_token['access_token'])
-                return client(authorizer=authorizer)
-            else:
-                raise ValueError('Attempted to load {} for user {}, but no '
-                                 'tokens existed with the name {}, only {}'
-                                 ''.format(client, user, token_name,
-                                           list(service_tokens.keys())))
-    return client()
+    token = load_globus_access_token(user, token_name)
+    if token:
+        return client(authorizer=globus_sdk.AccessTokenAuthorizer(token))
+    elif not require_authorized:
+        return client()
+    else:
+        raise ValueError(
+            'User {} has not been authorized for {}'.format(user, client))
+
+
+def load_auth_client(user):
+    return load_globus_client(user, globus_sdk.AuthClient,
+                              'auth.globus.org', require_authorized=True)
