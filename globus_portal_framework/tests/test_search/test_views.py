e2257358d87467bef798c138f1df4da0ea48930e
Nickolaus D. Saint <nick@globus.org>
2018-03-30 10:12:24 -0700

    Added many tests for tranfer and preview
    Also backwrote a few tests that were missed previously

--- globus_portal_framework/tests/test_search/test_views.py
+++ globus_portal_framework/tests/test_search/test_views.py
@@ -1,7 +1,19 @@
-from django.test import TestCase, Client
 from unittest import mock
+from urllib.parse import quote_plus
+
+from django.test import TestCase, Client
 from django.test.utils import override_settings
+from django.urls import reverse
+
+from globus_sdk import TransferAPIError
+
 from globus_portal_framework.tests import test_search
+from globus_portal_framework.tests.mocks import (
+    get_logged_in_client, MockTransferClient, MockTransferAPIError)
+
+from globus_portal_framework import (
+    PreviewException, PreviewPermissionDenied, PreviewNotFound,
+    PreviewServerError, PreviewBinaryData)
 
 
 class MockSearchGetSubject:
@@ -12,6 +24,10 @@ class SearchViewsTest(TestCase):
 
     def setUp(self):
         self.c = Client()
+        self.foo_ep = 'eae9d78d-b353-4fa7-9dbe-c60d681bc783'
+        # A valid subject url given our views
+        self.subject_url = quote_plus('globus://{}:{}'.format(self.foo_ep,
+                                                              'foo'))
 
     def test_index(self):
         r = self.c.get('/')
@@ -22,7 +38,7 @@ class SearchViewsTest(TestCase):
     @mock.patch('globus_sdk.SearchClient.get_subject')
     def test_detail(self, get_subject):
         get_subject.return_value = MockSearchGetSubject()
-        r = self.c.get('/detail/mysubject/')
+        r = self.c.get(reverse('detail', args=['mysubject']))
         self.assertEqual(r.status_code, 200)
 
     @mock.patch('globus_portal_framework.search.settings.SEARCH_MAPPER',
@@ -30,5 +46,51 @@ class SearchViewsTest(TestCase):
     @mock.patch('globus_sdk.SearchClient.get_subject')
     def test_detail_metadata(self, get_subject):
         get_subject.return_value = MockSearchGetSubject()
-        r = self.c.get('/detail-metadata/mysubject/')
+        r = self.c.get(reverse('detail-metadata', args=['mysubject']))
         self.assertEqual(r.status_code, 200)
+
+    @mock.patch('globus_portal_framework.search.views.get_helper_page_url',
+                mock.Mock())
+    @mock.patch('globus_sdk.SearchClient.get_subject')
+    @mock.patch('globus_sdk.TransferClient', MockTransferClient)
+    def test_detail_transfer(self, get_subject):
+        client, user = get_logged_in_client('mal', ['search.api.globus.org',
+                                                    'transfer.api.globus.org'])
+        get_subject.return_value = MockSearchGetSubject()
+        r = client.get(reverse('detail-transfer', args=[self.subject_url]))
+        self.assertEqual(r.status_code, 200)
+
+    @mock.patch('globus_portal_framework.search.views.preview')
+    @mock.patch('globus_sdk.SearchClient.get_subject')
+    @mock.patch('globus_sdk.TransferClient', MockTransferClient)
+    def test_detail_preview(self, get_subject, preview):
+        preview.return_value = mock.Mock()
+        client, user = get_logged_in_client('mal', ['search.api.globus.org',
+                                                    'transfer.api.globus.org'])
+        get_subject.return_value = MockSearchGetSubject()
+        r = client.get(reverse('detail-preview', args=[self.subject_url]))
+        self.assertEqual(len(r.context['messages']), 0)
+        self.assertEqual(r.status_code, 200)
+        self.assertTrue(preview.called)
+
+    @mock.patch('globus_portal_framework.search.views.log')
+    @mock.patch('globus_portal_framework.search.views.preview')
+    @mock.patch('globus_sdk.SearchClient.get_subject')
+    @mock.patch('globus_sdk.TransferClient', MockTransferClient)
+    def test_detail_preview_exceptions(self, get_subject, preview, log):
+        client, user = get_logged_in_client('mal', ['search.api.globus.org',
+                                                    'transfer.api.globus.org'])
+        get_subject.return_value = MockSearchGetSubject()
+        excs = (PreviewException, PreviewPermissionDenied, PreviewNotFound,
+                PreviewServerError, PreviewBinaryData)
+        for exc in excs:
+            if exc == PreviewServerError:
+                preview.side_effect = PreviewServerError(500, 'Serv Err')
+            else:
+                preview.side_effect = exc
+            r = client.get(reverse('detail-preview', args=[self.subject_url]))
+            self.assertEqual(len(r.context['messages']), 1)
+            self.assertEqual(r.status_code, 200)
+
+        # Once for each: ['UnexpectedError', 'ServerError']
+        self.assertEqual(log.error.call_count, 2)
--- globus_portal_framework/tests/test_transfer/test_utils.py
+++ globus_portal_framework/tests/test_transfer/test_utils.py
@@ -1,22 +1,44 @@
 from unittest import mock
-from django.test import TestCase, Client
+import json
+from django.test import TestCase, Client, RequestFactory
 from django.test.utils import override_settings
-from django.contrib.auth.models import AnonymousUser
+from django.contrib.auth.models import AnonymousUser, User
+from django.core.validators import URLValidator
+from django.core.exceptions import ValidationError
+
+import globus_sdk
+
 
 from globus_portal_framework.tests.mocks import (
-    MockGlobusClient, mock_user, globus_client_is_loaded_with_authorizer
+    MockGlobusClient, mock_user, globus_client_is_loaded_with_authorizer,
+    MockTransferClient, MockTransferAPIError
 )
 
 from globus_portal_framework.transfer.utils import (
-    load_transfer_client, transfer_file, parse_globus_url, preview
+    load_transfer_client, transfer_file, parse_globus_url, preview,
+    helper_page_transfer, get_helper_page_url, check_exists, is_file
 )
 
 
 class TransferUtilsTest(TestCase):
 
     def setUp(self):
+        self.factory = RequestFactory()
+        self.user = User.objects.create_user(
+            username='mal', email='mal@globus.org', password='globusrocks')
         self.foo_endpoint = '3274dd6e-4d2b-4d57-a94b-b2bf1bc2fad6'
         self.bar_endpoint = 'a17459fe-04a7-4620-a831-810d7190358b'
+
+    def globus_helper_page_request(self):
+        post_data = {
+            'endpoint_id': self.bar_endpoint,
+            'path': '/helper_page_path',
+            'label': 'my transfer'
+        }
+        request = self.factory.post('/transfer_stuff',
+                                    data=post_data)
+        request.user = self.user
+        return request
 
     @mock.patch('globus_sdk.TransferClient', MockGlobusClient)
     def test_load_search_client_with_anonymous_user(self):
@@ -34,6 +56,108 @@ class TransferUtilsTest(TestCase):
         user = mock_user('alice', ['search.api.globus.org'])
         with self.assertRaises(ValueError):
             load_transfer_client(user)
+
+    @mock.patch('globus_sdk.TransferAPIError', MockTransferAPIError)
+    @mock.patch('globus_portal_framework.transfer.utils.is_file')
+    def test_check_exists(self, is_file):
+        check_exists(self.user, self.foo_endpoint, 'path')
+        self.assertTrue(is_file.called)
+
+    @mock.patch('globus_sdk.TransferAPIError', MockTransferAPIError)
+    @mock.patch('globus_sdk.TransferClient')
+    def test_is_file_on_mock_file(self, tc):
+        # Transfer Client raises on existing files
+        tc.return_value = MockTransferClient(raise_on_ls=True)
+        user = mock_user('alice', ['transfer.api.globus.org'])
+        self.assertTrue(is_file(user, self.foo_endpoint, 'foo.txt'))
+
+    @mock.patch('globus_sdk.TransferAPIError', MockTransferAPIError)
+    @mock.patch('globus_sdk.TransferClient')
+    def test_is_file_on_mock_dir(self, tc):
+        # TC won't raise on dirs
+        tc.return_value = MockTransferClient(raise_on_ls=False)
+        user = mock_user('alice', ['transfer.api.globus.org'])
+        self.assertFalse(is_file(user, self.foo_endpoint, 'foo/'))
+
+    @mock.patch('globus_sdk.TransferAPIError', MockTransferAPIError)
+    @mock.patch('globus_sdk.TransferClient')
+    def test_is_file_raises_errors_properly(self, tc):
+        # We'll assume a TransferAPIError of type FOO, the worst kind of error
+        tc.return_value = MockTransferClient(raise_on_ls=True, exc_code='FOO')
+        user = mock_user('alice', ['transfer.api.globus.org'])
+        with self.assertRaises(globus_sdk.TransferAPIError):
+            is_file(user, self.foo_endpoint, 'bar', raises=True)
+
+        tc.return_value = MockTransferClient(raise_on_ls=True)
+        # No error should be raised here, due to the exception indicating
+        # the file exists
+        self.assertTrue(is_file(user, self.foo_endpoint, 'bar', raises=True))
+
+    def test_get_helper_page_url(self):
+        url = get_helper_page_url('http://example.com/transfer_callback',
+                                  cancel_url='http://example.com/choose_file',
+                                  folder_limit=234,
+                                  file_limit=934,
+                                  label='bestest-transfer')
+        # Check that these things are generally making it into the url. Don't
+        # bother checking for URL encoded stuff.
+        self.assertIn('234', url)
+        self.assertIn('934', url)
+        self.assertIn('bestest-transfer', url)
+        self.assertIn('transfer_callback', url)
+        self.assertIn('choose_file', url)
+        check_url_valid = URLValidator()
+        check_url_valid(url)
+
+    def test_get_helper_page_url_raises_error_for_bad_callback(self):
+        with self.assertRaises(ValidationError):
+            get_helper_page_url('not_a_valid_url.com')
+
+    def test_get_helper_page_url_raises_error_for_bad_cancel_url(self):
+        with self.assertRaises(ValidationError):
+            get_helper_page_url('http://example.com/transfer_callback',
+                                cancel_url='you_shall_not_go_back.edu')
+
+    @mock.patch('globus_portal_framework.transfer.utils.transfer_file')
+    def test_helper_page_transfer(self, transfer_file):
+        request = self.globus_helper_page_request()
+        helper_page_transfer(request, self.foo_endpoint, '/path',
+                             helper_page_is_dest=True)
+        args, kwargs = transfer_file.call_args
+        u, src_ep, src_path, dest_ep, dest_path, label = args
+        self.assertEqual(u, self.user)
+        self.assertEqual(src_ep, self.foo_endpoint)
+        self.assertEqual(src_path, '/path')
+        self.assertEqual(dest_ep, self.bar_endpoint)
+        self.assertEqual(dest_path, '/helper_page_path')
+        self.assertEqual(label, 'my transfer')
+
+    @mock.patch('globus_portal_framework.transfer.utils.transfer_file')
+    def test_helper_page_transfer_helper_page_as_source(self, transfer_file):
+        request = self.globus_helper_page_request()
+        helper_page_transfer(request, self.foo_endpoint, '/path',
+                             helper_page_is_dest=False)
+        args, kwargs = transfer_file.call_args
+        # Source and dest and swapped
+        _, dest_ep, dest_path, src_ep, src_path, _ = args
+        self.assertEqual(src_ep, self.foo_endpoint)
+        self.assertEqual(src_path, '/path')
+        self.assertEqual(dest_ep, self.bar_endpoint)
+        self.assertEqual(dest_path, '/helper_page_path')
+
+    @mock.patch('globus_portal_framework.transfer.utils.transfer_file')
+    def test_helper_page_raises_error_for_gets(self, transfer_file):
+        request = self.factory.get('/transfer_stuff')
+        request.user = self.user
+        with self.assertRaises(ValueError):
+            helper_page_transfer(request, self.foo_endpoint, '/path')
+
+    @mock.patch('globus_portal_framework.transfer.utils.transfer_file')
+    def test_helper_page_raises_error_for_anonymous_user(self, transfer_file):
+        request = self.globus_helper_page_request()
+        request.user = AnonymousUser
+        with self.assertRaises(ValueError):
+            helper_page_transfer(request, self.foo_endpoint, '/path')
 
     @mock.patch('globus_sdk.TransferClient')
     @mock.patch('globus_sdk.TransferData')
@@ -69,3 +193,9 @@ class TransferUtilsTest(TestCase):
         foo_file = 'globus://{}:{}'.format(self.foo_endpoint[:35], '/foo.txt')
         with self.assertRaises(ValueError):
             parse_globus_url(foo_file)
+
+    # def test_preview(self):
+    #     """
+    #     Todo: This
+    #     """
+    #     pass
