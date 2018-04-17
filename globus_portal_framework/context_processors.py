16b8aed0981f97ad17565faad99518b4495ca2ea
Nickolaus D. Saint <nick@globus.org>
2018-04-17 14:08:12 -0700

    Transfer and Preview now auto-disable until they are configured correctly

--- globus_portal_framework/context_processors.py
+++ globus_portal_framework/context_processors.py
@@ -2,8 +2,19 @@ from django.conf import settings
 
 
 def globals(request):
+    auth_enabled = bool('social_django' in settings.INSTALLED_APPS)
+    scopes = getattr(settings, 'SOCIAL_AUTH_GLOBUS_SCOPE', [])
+    transfer_scope_set = any(['transfer.api.globus.org' in scope
+                              for scope in scopes])
+    preview_setup = bool(
+        getattr(settings, 'GLOBUS_HTTP_ENDPOINT', False) and
+        getattr(settings, 'PREVIEW_TOKEN_NAME', False)
+    )
+
     return {
         'project_title': getattr(settings, 'PROJECT_TITLE',
                                  'Globus Portal Framework'),
-        'auth_enabled': bool('social_django' in settings.INSTALLED_APPS)
+        'auth_enabled': auth_enabled,
+        'transfer_enabled': auth_enabled and transfer_scope_set,
+        'preview_enabled': auth_enabled and preview_setup
     }
