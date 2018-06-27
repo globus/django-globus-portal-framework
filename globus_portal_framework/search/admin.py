3321b580e1fa31ed0a6cd69d5f472e79bf95743d
Nickolaus D. Saint <nick@globus.org>
2018-06-27 08:41:23 -0700

    Made admin display better for minids

--- globus_portal_framework/search/admin.py
+++ globus_portal_framework/search/admin.py
@@ -5,7 +5,7 @@ from globus_portal_framework.search.models import Minid
 
 @admin.register(Minid)
 class MinidAdmin(admin.ModelAdmin):
-    list_display = ('id', 'get_users')
+    list_display = ('id', 'description', 'get_users')
 
     def get_users(self, minid):
-        return minid.users.all()
+        return ','.join([u.username for u in minid.users.all()])
