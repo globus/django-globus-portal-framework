2d27ba96987b3e921750fbffb352e0c3881dad8c
Nickolaus D. Saint <nick@globus.org>
2018-08-31 14:14:55 -0700

    Small default template fixes, added index data to default context processor

--- globus_portal_framework/context_processors.py
+++ globus_portal_framework/context_processors.py
@@ -1,6 +1,6 @@
 from django.conf import settings
 from django.urls import resolve
-from globus_portal_framework import get_index
+from globus_portal_framework import get_index, IndexNotFound
 
 
 def globals(request):
@@ -9,11 +9,17 @@ def globals(request):
     transfer_scope_set = any(['transfer.api.globus.org' in scope
                               for scope in scopes])
     index = resolve(request.path).kwargs.get('index')
+    # Don't throw IndexNotFound exceptions here, they are hard to debug
+    try:
+        index_data = get_index(index)
+    except IndexNotFound:
+        index_data = {}
     return {'globus_portal_framework': {
             'project_title': getattr(settings, 'PROJECT_TITLE',
                                      'Globus Portal Framework'),
             'auth_enabled': auth_enabled,
             'transfer_enabled': auth_enabled and transfer_scope_set,
+            'index_data': index_data,
             'index': index,
         }
     }
