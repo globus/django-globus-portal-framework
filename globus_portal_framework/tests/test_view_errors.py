bc5234dfcad846a0169f57a5ee54fbd88886efae
Nickolaus D. Saint <nick@globus.org>
2019-12-04 10:20:38 -0800

    Fixed 404 and 500 tests

--- globus_portal_framework/tests/test_view_errors.py
+++ globus_portal_framework/tests/test_view_errors.py
@@ -2,18 +2,16 @@ from unittest import mock
 
 from django.test import SimpleTestCase, override_settings
 from django.urls import path
+from django.views.defaults import server_error
 
 from globus_portal_framework.urls import urlpatterns
-from globus_portal_framework.views import handler500, handler404
-
 
 urlpatterns += [
-    path('not-a-real-page', handler404),
-    path('exception-view/', handler500)
+    path('exception-view/', server_error)
 ]
 
 
-@override_settings(ROOT_URLCONF=__name__)
+@override_settings(ROOT_URLCONF=__name__, DEBUG=False)
 class CustomErrorHandlerTests(SimpleTestCase):
 
     @mock.patch('globus_portal_framework.views.log', mock.Mock())
@@ -27,8 +25,3 @@ class CustomErrorHandlerTests(SimpleTestCase):
         response = self.client.get('/exception-view/')
         # Make assertions on the response here. For example:
         self.assertContains(response, "That's an error.", status_code=500)
-
-    @mock.patch('globus_portal_framework.views.log')
-    def test_500_logs_exception(self, log):
-        handler500(self.client.get('/exception-view/'), Exception())
-        self.assertTrue(log.exception.called)
