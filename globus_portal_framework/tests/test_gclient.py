a00a17d39a6974453876b9bb40d54abae163df84
Nickolaus D. Saint <nick@globus.org>
2020-09-09 12:13:09 -0700

    Added check for Globus Group Resource Server name change

--- globus_portal_framework/tests/test_gclient.py
+++ globus_portal_framework/tests/test_gclient.py
@@ -1,5 +1,6 @@
 from unittest import mock
 from datetime import timedelta
+import requests
 
 from django.test import TestCase
 from django.test.utils import override_settings
@@ -11,13 +12,14 @@ import globus_sdk
 from globus_portal_framework import (
     load_globus_client, load_search_client, load_transfer_client,
 
-    ExpiredGlobusToken
+    ExpiredGlobusToken, GlobusPortalException,
 )
 
-from globus_portal_framework.gclients import revoke_globus_tokens
+from globus_portal_framework.gclients import revoke_globus_tokens, get_user_groups
 
 from globus_portal_framework.tests.mocks import (
-    MockGlobusClient, mock_user, globus_client_is_loaded_with_authorizer
+    MockGlobusClient, mock_user, globus_client_is_loaded_with_authorizer,
+    get_logged_in_client,
 )
 
 
@@ -112,3 +114,37 @@ class GlobusPortalFrameworkUtilsTests(TestCase):
         revoke_globus_tokens(user)
         # Twice for each service access_token, and refresh_token
         self.assertEqual(log.exception.call_count, 3)
+
+    @mock.patch('requests.get')
+    def test_get_groups_old_token_name(self, rget):
+        # get user with public groups scope
+        client, user = get_logged_in_client('mal', ['04896e9e-b98e-437e-'
+                                                    'becd-8084b9e234a0'])
+        get_user_groups(user)
+        self.assertTrue(user.is_authenticated)
+        self.assertTrue(rget.called)
+        self.assertTrue(rget.return_value.json.called)
+
+    @mock.patch('requests.get')
+    def test_get_groups_token_name(self, rget):
+        # get user with public groups scope
+        client, user = get_logged_in_client('mal', ['groups.api.globus.org'])
+        get_user_groups(user)
+        self.assertTrue(user.is_authenticated)
+        self.assertTrue(rget.called)
+        self.assertTrue(rget.return_value.json.called)
+
+    @mock.patch('requests.get')
+    def test_get_groups_bad_token_name(self, rget):
+        # get user with public groups scope
+        client, user = get_logged_in_client('mal', ['incorrect.api.globus.org'])
+        with self.assertRaises(ValueError):
+            get_user_groups(user)
+
+    @mock.patch('requests.get')
+    def test_get_groups_err_response(self, rget):
+        # get user with public groups scope
+        client, user = get_logged_in_client('mal', ['groups.api.globus.org'])
+        rget.return_value.raise_for_status.side_effect = requests.HTTPError()
+        with self.assertRaises(GlobusPortalException):
+            get_user_groups(user)
