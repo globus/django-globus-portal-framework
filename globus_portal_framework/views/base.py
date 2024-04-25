bc7e51756742257bd9b24bd224ccc32e156f95cc
Nickolaus D. Saint <nick@globus.org>
2024-04-25 13:40:01 -0700

    docs: Document base DGPF views

--- globus_portal_framework/views/base.py
+++ globus_portal_framework/views/base.py
@@ -4,10 +4,13 @@ from urllib.parse import urlparse
 from collections import OrderedDict
 from json import dumps
 import globus_sdk
+import django
 from django.conf import settings
 from django.contrib import messages
 from django.shortcuts import render, redirect
 from django.urls import reverse
+from django.http import HttpRequest, HttpResponse, HttpResponseRedirect
+from django.template.response import TemplateResponse
 from django.views.decorators.csrf import csrf_exempt
 from django.views.defaults import server_error, page_not_found
 from django.contrib.auth import logout as django_logout
@@ -22,7 +25,55 @@ from globus_portal_framework import (
 log = logging.getLogger(__name__)
 
 
-def index_selection(request):
+def index_selection(request: HttpRequest) -> django.template.response.TemplateResponse:
+    """
+    This is usually the root `/` page for the portal for Globus Portal Framework. Users
+    are first directed to this page to choose a Globus Search index. If the portal is
+    configured with groups, they may also be prompted to check the groups page to ensure
+    they have access to view the portal.
+
+    :param request: The Django request object
+    :returns: A rendered Django Template View
+
+    **Templates**:
+
+    Chooses the following templates in order of precedence if they exist, determined
+    by ``settings.BASE_TEMPLATES``:
+
+    * ``templates/globus-portal-framework/v2/index-selection.html``
+    * *Default DGPF Template*
+
+    **Context**:
+
+    This view creates the following context:
+
+    * ``search_indexes`` - The entire block defined in settings.SEARCH_INDEXES
+    * ``allowed_groups`` - The entire block defined in settings.SOCIAL_AUTH_GLOBUS_ALLOWED_GROUPS
+
+    **Context Example**:
+
+    .. code-block:: python
+
+        {
+            'allowed_groups': [
+                {
+                    'name': 'My Group 1',
+                    'uuid': '08d8cd36-dd9d-11ee-8849-b93550bcf92a'
+                }
+            ],
+            'search_indexes': {
+                'perfdata': {
+                    'facets': [...],
+                    'fields': [...]
+                    'filter_match': 'match-all',
+                    'name': 'Performance Data',
+                    'template_override_dir': 'perfdata',
+                    'uuid': '5e83718e-add0-4f06-a00d-577dc78359bc'
+                }
+            }
+        }
+
+    """
     context = {
         'search_indexes': get_setting('SEARCH_INDEXES'),
         'allowed_groups': getattr(settings,
@@ -32,26 +83,55 @@ def index_selection(request):
                   context)
 
 
-def search_about(request, index):
+def search_about(request: HttpRequest, index: str):
+    """
+    The Search About view is intended to show basic information about a Globus Search
+    Index. It is by default accessible via the search tab located here:
+
+    ``templates/globus-portal-framework/v2/components/search-nav.html``
+
+    :param request: The Django request object
+    :param index: The URL string identifying the Search Index
+    :returns: A rendered Django Template View
+
+    **Templates**:
+
+    Chooses the following templates in order of precedence if they exist, determined
+    by ``settings.BASE_TEMPLATES`` and ``settings.SEARCH_INDEXES.<index>.template_override_dir``
+    in :ref:`search_settings_reference`:
+
+    * ``templates/my-index/globus-portal-framework/v2/search-about.html``
+    * ``templates/globus-portal-framework/v2/search-about.html``
+    * *Default DGPF Template*
+
+    **Context**:
+
+    This view has not context.
+    """
     tvers = gsearch.get_template_path('search-about.html', index=index)
     return render(request, gsearch.get_template(index, tvers), {})
 
 
-def search(request, index):
+def search(request: HttpRequest, index: str) -> django.template.response.TemplateResponse:
     """
     Search the 'index' with the queryparams 'q' for query, 'filter.<filter>'
     for facet-filtering, 'page' for pagination If the user visits this
     page again without a search query, we auto search for them
     again using their last query. If the user is logged in, they will
     automatically do a credentialed search for Globus Search to return
-    confidential results. If more results than settings.SEARCH_RESULTS_PER_PAGE
-    are returned, they are paginated (Globus Search does the pagination, we
-    only do the math to calculate the offset).
+    confidential results (results where user is listed within ``visible_to``).
+    If more results than settings.SEARCH_RESULTS_PER_PAGE are returned, they
+    are paginated.
 
-    Query Params
-    q: key words for the users search. Ex: 'q=foo*' will search for 'foo*'
+    :param request: The Django request object
+    :param index: The URL string identifying the Search Index
+    :returns: A rendered Django Template View
 
-    filter.: Filter results on facets defined in settings.SEARCH_SCHEMA. The
+    **Query Params**:
+
+    ``q`` -- key words for the users search. Ex: 'q=foo*' will search for 'foo*'
+
+    ``filter.`` -- Filter results on facets defined in settings.SEARCH_SCHEMA. The
     syntax for filters using query params is:
     '?filter.<filter_type>=<filter_value>, where 'filter.<filter_type>' is
     defined in settings.SEARCH_SCHEMA and <filter_value> is any value returned
@@ -59,29 +139,56 @@ def search(request, index):
     define a filter 'mdf.elements' in our schema, and use it to filter all
     results containing H (Hydrogen).
 
-    page: Page of the search results. Number of results displayed per page is
+    ``page`` -- Page of the search results. Number of results displayed per page is
     configured in settings.SEARCH_RESULTS_PER_PAGE, and number of pages can be
     controlled with settings.SEARCH_MAX_PAGES.
 
-    Templates:
-    The resulting page is templated using the 'search.html' template, and
-    'search-results.html' and 'search-facets.html' template components. The
-    context required for searches is shown here:
+    **Templates**:
+
+    Chooses the following templates in order of precedence if they exist, determined
+    by ``settings.BASE_TEMPLATES`` and ``settings.SEARCH_INDEXES.<index>.template_override_dir``
+    in :ref:`search_settings_reference`:
+
+    * ``templates/my-index/globus-portal-framework/v2/search.html``
+    * ``templates/globus-portal-framework/v2/search.html``
+    * *Default DGPF Template*
+
+    **Context** - This view generates the following context:
+
+    * ``count`` The number of search results shown
+    * ``offset`` The offset of within the total search results, if the user has attempted to
+      view the next page.
+    * ``total`` The total results the search returned
+    * ``pagination`` A dict containing pagination information about current and available pages
+      of search results
+    * ``facets`` Facets returned by ``get_search_facets`` in :ref:`gsearch_reference`
+      DGPF does extra work to ensure facets are returned in the same order they are
+      listed in SEARCH_RESULTS. ``SEARCH_INDEXES.facet_modifiers`` (see :ref:`facet_modifiers`)
+      can be used to modify data contained here.
+    * ``search_results`` A list of results (controlled by settings.SEARCH_RESULTS_PER_PAGE) which
+      contains two additional items for each search result:
+            * ``subject``: The Globus Search subject identifying the search result id
+            * ``fields``: An object containing DGPF fields (:ref:`configuring_fields`). Raw results are
+              always available via `all`.
 
 
-    .. code-bock:: python
+    .. code-block:: python
 
         {
             'search': {
+                'count': 2,
+                'offset': 0,
+                'total': 2,
+                'pagination': {'current_page': 1, 'pages': [{'number': 1}]},
                 'facets': [
                     {'buckets': [{'field_name': 'mdf.resource_type',
                                 'value': 'record'}],
                     'name': 'Resource Type'},
                     <More Facets>...
                 ],
-                'pagination': {'current_page': 1, 'pages': [{'number': 1}]},
                 'search_results': [
                 {
+                    'all': [<raw subject>],
                     'subject': '<Globus Search Subject>',
                     'fields': {
                         'titles': {'field_name': 'titles',
@@ -140,27 +247,52 @@ def search_debug_detail(request, index, subject):
     return render(request, gsearch.get_template(index, tvers), sub)
 
 
-def detail(request, index, subject):
+def detail(request: HttpRequest, index: str, subject: str) ->  django.template.response.TemplateResponse:
     """
-    Load a page for showing details for a single search result. The data is
-    exactly the same as the entries loaded by the index page in the
-    'search_results'. The template is ultimately responsible for which fields
-    are displayed. The only real functional difference between the index page
-    and the detail page is that it displays only a single result. The
-    detail-overview.html template is used to render the page.
+    Load a page for showing details for a single search result (subject). This view fetches
+    the subject in the URL from Globus Search, and runs it through DGPF Fields (:ref:`configuring_fields`).
+    to produce context ready to render. The Detail page is intended to show all information about
+    a Globus Search Subject, compared with the search view which is only intended to show minimum relavent
+    information and hide unnecessary detail.
 
-    Example request:
-    http://myhost/detail/<subject>
+    :param request: The Django request object
+    :param index: The URL string identifying the Search Index
+    :param subject: The Globus Search Subject to be displayed
+    :returns: A rendered Django Template View
+
+
+    **Templates**:
+
+    Chooses the following templates in order of precedence if they exist, determined
+    by ``settings.BASE_TEMPLATES`` and ``settings.SEARCH_INDEXES.<index>.template_override_dir``
+    in :ref:`search_settings_reference`:
+
+    * ``templates/my-index/globus-portal-framework/v2/detail-overview.html``
+    * ``templates/globus-portal-framework/v2/detail-overview.html``
+    * *Default DGPF Template*
+
+    **Example Request**:
+    ``https://myhost/<myindex>/detail/<subject>/``
+
+    **Context**
+
+    * ``subject`` The quoted subject safe for being included in a URL
+    * ``fields`` A dict of all configured fields (:ref:`configuring_fields`).
+        * Note: `all` is always included for the raw subject data.
 
     Example context:
-    {'subject': '<Globus Search Subject>',
-     'fields': {
+
+    .. code-block::
+
+        {
+            'subject': '<Globus Search Subject>',
+            'fields': {
+                'all': [<list of raw entries>]
                 'titles': {'field_name': 'titles', 'value': '<Result Title>'},
                 'version': {'field_name': 'version', 'value': '0.3.2'},
-                '<field_name>': {'field_name': '<display_name>', 'value':
-                                                            '<field_value>'}
-                }
-    }
+                '<field_name>': {'field_name': '<display_name>', 'value': '<field_value>'}
+            }
+        }
     """
     tvers = gsearch.get_template_path('detail-overview.html', index=index)
     template = gsearch.get_template(index, tvers)
@@ -170,6 +302,9 @@ def detail(request, index, subject):
 
 @csrf_exempt
 def detail_transfer(request, index, subject):
+    """
+    Deprecated. Will be removed in a future version.
+    """
     context = gsearch.get_subject(index, subject, request.user)
     task_url = 'https://app.globus.org/activity/{}/overview'
     if request.user.is_authenticated:
@@ -215,6 +350,9 @@ def detail_transfer(request, index, subject):
 
 
 def detail_preview(request, index, subject, endpoint=None, url_path=None):
+    """
+    Deprecated. Will be removed in a future version.
+    """
     context = gsearch.get_subject(index, subject, request.user)
     try:
         scope = request.GET.get('scope')
@@ -236,12 +374,16 @@ def detail_preview(request, index, subject, endpoint=None, url_path=None):
     return render(request, gsearch.get_template(index, tvers), context)
 
 
-def logout(request, next='/'):
+def logout(request: HttpRequest, next: str = '/') -> HttpResponseRedirect:
     """
-    Revoke the users tokens and pop their Django session. Users will be
-    redirected to the query parameter 'next' if it is present. If the 'next'
-    query parameter 'next' is not present, the parameter next will be used
-    instead.
+    Revoke the users tokens and pop their Django session, then redirect
+    the user.
+
+    :param request: The Django Request
+    :param next: Location the user will be redirected after logout. Defaults to `/`.
+        Can be specified by URL parameter or Query Parameter. Query Parameter takes
+        precedence.
+    :returns: django.http.HttpResponseRedirect
     """
     if request.user.is_authenticated:
         gclients.revoke_globus_tokens(request.user)
@@ -249,18 +391,59 @@ def logout(request, next='/'):
     return redirect(request.GET.get('next', next))
 
 
-def allowed_groups(request):
+def allowed_groups(request: HttpRequest) -> django.template.response.TemplateResponse:
     """
-    The groups view shows a user a list of groups that can be used to request
-    access to the server, if using SOCIAL_AUTH_GLOBUS_GROUPS_ALLOWED.
-    SOCIAL_AUTH_GLOBUS_GROUPS_ALLOWED prevents access to authenticated
-    resources globally unless a user is within the groups allowed.
+    Show available Globus Groups configured in settings.SOCIAL_AUTH_GLOBUS_ALLOWED_GROUPS.
 
-    Note: This is different than securing the visible_to field on Globus Search
-    records. Users may not even login unless they are in the allowlist. If a
-    user is allowlist and able to login, they still may not be able to view
-    records in Globus Search if the Globus Search records are configured with
-    a different group on the records' visible_to.
+    .. note::
+
+    This view is typically only used when groups are configured. If no groups
+    are configured all users will be allowed access and links to this view will not be
+    listed by default (although it is still accessible in the URLs).
+
+    .. warning::
+
+    This view **only** controls access to the portal. Results on Globus Search records
+    are controlled via ``visible_to`` per-search-result. Restricting who can login to
+    your portal does not stop users from being able to access publicly listed search
+    results via another Gloubs Application!
+
+    **Parameters**:
+
+    :param request: The Django Request object
+    :returns: A rendered Django Template View
+
+    **Templates**:
+
+    Chooses the following templates in order of precedence if they exist, determined
+    by ``settings.BASE_TEMPLATES`` and ``settings.SEARCH_INDEXES.<index>.template_override_dir``
+    in :ref:`search_settings_reference`:
+
+    * ``templates/my-index/globus-portal-framework/v2/allowed-groups.html``
+    * ``templates/globus-portal-framework/v2/allowed-groups.html``
+    * *Default DGPF Template*
+
+    **Context**
+
+    * ``allowed_groups`` -- A list of groups a user must have access to (at least one) to access
+        the portal.
+            * ``is_member`` -- True if the user is a member of this group
+            * ``name`` -- The human readable name of this group
+            * ``uuid`` -- The Globus uuid if this group.
+
+    Example context:
+
+    .. code-block::
+
+        {
+            'allowed_groups': [
+                {
+                    'is_member': True,
+                    'name': 'My Group 1',
+                    'uuid': '08d8cd36-dd9d-11ee-8849-b93550bcf92a'
+                }
+            ]
+        }
     """
     # Get the local portal allowlist. If there isn't a setting on the local
     # portal, don't restrict users. Copy the list so we can modify it.
